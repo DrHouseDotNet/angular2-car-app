18ee79bb4422f1525ac98b46b43e5bf497fa8ef1
Nicolas Takashi <Nicolas Takashi>
2016-01-21 00:06:40 -0200

    Adicionando master detail pattern

--- app/ts/car-master.component.ts
+++ app/ts/car-master.component.ts
@@ -8,15 +8,28 @@ interface Car {
 
 @Component({
     selector: 'my-car',
-    templateUrl:'app/view/car-master.html'
+    templateUrl: 'app/view/car-master.html'
 })
 
 
 export class CarComponent {
     public title: string = "Cadastro de veículos";
-    public car: Car = {
-        id: 1,
-        brand: 'BMW',
-        model: 'X6'
+    public selectedCar: Car;
+    public cars: Car[] = Cars;
+    
+    public onSelect(car: Car): void {
+        this.selectedCar = car;
     }
+    
+    
 }
+
+var Cars: Car[] = [
+    { id: 1, brand: "BMW", model: "X6" },
+    { id: 2, brand: "BMW", model: "X1" },
+    { id: 3, brand: "Volkswagen", model: "Golf GTI" },
+    { id: 4, brand: "Chevrolet", model: "Cruze" },
+    { id: 5, brand: "Ford", model: "Fusion" },
+    { id: 6, brand: "Fiat", model: "Bravo" },
+    { id: 7, brand: "Mercedes", model: "C180" },
+]
