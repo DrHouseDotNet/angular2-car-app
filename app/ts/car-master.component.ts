4307107bf41bb19bf8c1577f05c30842b7c050c4
Nicolas Takashi <Nicolas Takashi>
2016-01-31 21:33:46 -0200

    separando responsabilidades

--- app/ts/car-master.component.ts
+++ app/ts/car-master.component.ts
@@ -1,35 +1,20 @@
 import {Component} from 'angular2/core'
-
-interface Car {
-    id: number;
-    brand: string;
-    model: string;
-}
+import {Car} from './car'
+import {CarDetailComponent} from './car-detail.component'
+import {Cars} from './mock-car'
 
 @Component({
     selector: 'my-car',
-    templateUrl: 'app/view/car-master.html'
+    templateUrl: 'app/view/car-master.html',
+    directives: [CarDetailComponent]
 })
-
 
 export class CarComponent {
     public title: string = "Cadastro de veículos";
-    public selectedCar: Car;
     public cars: Car[] = Cars;
-    
+    public selectedCar: Car;
+
     public onSelect(car: Car): void {
         this.selectedCar = car;
     }
-    
-    
 }
-
-var Cars: Car[] = [
-    { id: 1, brand: "BMW", model: "X6" },
-    { id: 2, brand: "BMW", model: "X1" },
-    { id: 3, brand: "Volkswagen", model: "Golf GTI" },
-    { id: 4, brand: "Chevrolet", model: "Cruze" },
-    { id: 5, brand: "Ford", model: "Fusion" },
-    { id: 6, brand: "Fiat", model: "Bravo" },
-    { id: 7, brand: "Mercedes", model: "C180" },
-]
